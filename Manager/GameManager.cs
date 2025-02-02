91554d66bd8c249a812fbc0d9e451d0cb8ec3927
Jaewon <dhwodnjs0827@naver.com>
2025-02-02 17:54:58 +0900

    Singleton, GameManager 클래스 추가
    Manager 클래스를 만들기 위한 Singleton 클래스 추가
    게임을 관리해줄 GameManager 클래스 추가
    게임 영역 그려주는 메서드 추가

---
+++ Manager/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaDungeon.Manager
+{
+    public class GameManager : Singleton<GameManager>
+    {
+        public void DrawScreen()
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.Write('┌');
+            for (int i = 1; i <= 80; i++)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.Write('─');
+            }
+            Console.SetCursorPosition(81, 0);
+            Console.Write('┐');
+
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write('│');
+                Console.SetCursorPosition(81, i);
+                Console.Write('│');
+            }
+
+            Console.SetCursorPosition(0, 21);
+            Console.Write('└');
+            for (int i = 1; i <= 80; i++)
+            {
+                Console.SetCursorPosition(i, 21);
+                Console.Write('─');
+            }
+            Console.SetCursorPosition(81, 21);
+            Console.Write('┘');
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-﻿namespace SpartaDungeon
+﻿using SpartaDungeon.Manager;
+
+namespace SpartaDungeon
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GameManager gameManager = GameManager.Instance;
+            gameManager.DrawScreen();
         }
     }
 }
